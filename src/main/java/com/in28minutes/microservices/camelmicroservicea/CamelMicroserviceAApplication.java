a0fab12e5938a53e9540e295a179d3991b1af7a3
pratmodi <pratmodi@gmail.com>
2022-12-18 10:25:29 +0530

    Committed microservice a

--- src/main/java/com/in28minutes/microservices/camelmicroservicea/CamelMicroserviceAApplication.java
+++ src/main/java/com/in28minutes/microservices/camelmicroservicea/CamelMicroserviceAApplication.java
@@ -1,12 +1,10 @@
 package com.in28minutes.microservices.camelmicroservicea;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 public class CamelMicroserviceAApplication {
@@ -18,6 +16,7 @@ public class CamelMicroserviceAApplication {
 	
 	public static void main(String[] args) {
 	//	ctx = new AnnotationConfigApplicationContext(CamelMicroserviceAApplication.class);
+		
 		SpringApplication.run(CamelMicroserviceAApplication.class, args);
 	}
 	
